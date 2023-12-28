d61d97f26b4c080ad7cb04a60fc844e018291bf8
KariTToKaRo <carolinacdlp@gmail.com>
2023-12-28 13:56:54 +0100

    responsives chek
    falta: cuadrar la foto dntro de los cuadros
    
    que funcione el js del iconito favorito

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*iconito de favoritos on/off*/
+$(document).ready(function(){
+ 
+  $(".shoppingUtil >  img").mouseover(function(){
+       console.log("ruta="+$(this).attr("src"));
+       var ruta = $(this).attr("src").replace("StarA","StarB");
+       $(this).attr("src",ruta);
+   });
+
+   $(".shoppingUtil >  img").mouseout(function(){
+       console.log("ruta="+$(this).attr("src"));
+       var ruta = $(this).attr("src").replace("StarB","StarA");
+       $(this).attr("src",ruta);
+   });
+
+       
+});
